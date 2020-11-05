3f496e46cd14a2609f1b9ae888ca004ca071910f
divyansh49 <divyanshupadhyay49@gmail.com>
2020-11-06 01:20:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/android/mlgear/image/ObjectDetectionActivity.kt
@@ -0,0 +1,12 @@
+package com.android.mlgear.image
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.mlgear.R
+
+class ObjectDetectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_object_detection)
+    }
+}
